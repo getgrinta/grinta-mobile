68be16294fe9c99279af1c7a0d2e2b356bd4ffaa
Aleksander Lorenc <thaxsillion@gmail.com>
2025-02-07 19:18:42 +0100

    Initial Commit

---
+++ grinta/grintaApp.swift
@@ -0,0 +1,17 @@
+//
+//  grintaApp.swift
+//  grinta
+//
+//  Created by Aleksander Lorenc on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct grintaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ grintaTests/grintaTests.swift
@@ -0,0 +1,16 @@
+//
+//  grintaTests.swift
+//  grintaTests
+//
+//  Created by Aleksander Lorenc on 2/7/25.
+//
+
+import Testing
+
+struct grintaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
